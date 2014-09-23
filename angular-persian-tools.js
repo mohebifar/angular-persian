62c9fe1183c40b6e06e154e06ab43d06a8c7ec5c
Mohamad Mohebifar <netfars@gmail.com>
2014-09-24 01:13:22 +0330

    Create angular-persian-tools.js

---
+++ angular-persian-tools.js
@@ -0,0 +1,17 @@
+angular.module('ngPersianTools', []).filter('pnumber', function () {
+    return function ($string) {
+
+        $string = str_replace('0','۰',$string);
+        $string = str_replace('1','۱',$string);
+        $string = str_replace('2','۲',$string);
+        $string = str_replace('3','۳',$string);
+        $string = str_replace('4','۴',$string);
+        $string = str_replace('5','۵',$string);
+        $string = str_replace('6','۶',$string);
+        $string = str_replace('7','۷',$string);
+        $string = str_replace('8','۸',$string);
+        $string = str_replace('9','۹',$string);
+
+        return $string;
+    }
+});
