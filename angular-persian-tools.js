f723262624fa6f52fcbc27c96f6e78a1c695149a
Mohamad Mohebifar <netfars@gmail.com>
2014-09-24 02:03:13 +0330

    Fix Problems.

--- angular-persian-tools.js
+++ angular-persian-tools.js
@@ -2,21 +2,23 @@ angular.module('ngPersianTools', []).filter('pnumber', function () {
     return function ($string) {
 
         if (typeof $string === 'string' || isFinite($string)) {
-
-            $string = str_replace('0', '۰', $string);
-            $string = str_replace('1', '۱', $string);
-            $string = str_replace('2', '۲', $string);
-            $string = str_replace('3', '۳', $string);
-            $string = str_replace('4', '۴', $string);
-            $string = str_replace('5', '۵', $string);
-            $string = str_replace('6', '۶', $string);
-            $string = str_replace('7', '۷', $string);
-            $string = str_replace('8', '۸', $string);
-            $string = str_replace('9', '۹', $string);
+            $string = $string.toString();
+            
+            $string = $string.replace('0', '۰');
+            $string = $string.replace('1', '۱');
+            $string = $string.replace('2', '۲');
+            $string = $string.replace('3', '۳');
+            $string = $string.replace('4', '۴');
+            $string = $string.replace('5', '۵');
+            $string = $string.replace('6', '۶');
+            $string = $string.replace('7', '۷');
+            $string = $string.replace('8', '۸');
+            $string = $string.replace('9', '۹');
 
         }
 
         return $string;
     }
 });
+
 
