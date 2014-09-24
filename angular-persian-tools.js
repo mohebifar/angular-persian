6dfb97eafb704c99f3bb816a183715784f68a4e3
Hadi Mostafapour <info@bluetec.ir>
2014-09-24 18:41:36 +0330

    Update angular-persian-tools.js
    Recode numbers replacing functionality

--- angular-persian-tools.js
+++ angular-persian-tools.js
@@ -2,23 +2,21 @@ angular.module('ngPersianTools', []).filter('pnumber', function () {
     return function ($string) {
 
         if (typeof $string === 'string' || isFinite($string)) {
-            $string = $string.toString();
             
-            $string = $string.replace('0', '۰');
-            $string = $string.replace('1', '۱');
-            $string = $string.replace('2', '۲');
-            $string = $string.replace('3', '۳');
-            $string = $string.replace('4', '۴');
-            $string = $string.replace('5', '۵');
-            $string = $string.replace('6', '۶');
-            $string = $string.replace('7', '۷');
-            $string = $string.replace('8', '۸');
-            $string = $string.replace('9', '۹');
+            // string
+            $string = $string + '';
+            
+            // Numbers
+            $numbers = {0: '۰', 1: '۱', 2: '۲', 3: '۳', 4: '۴', 5: '۵', 6: '۶', 7: '۷', 8: '۸', 9: '۹'};
+            
+            // Replace
+            var regex = new RegExp(Object.keys($numbers).join('|'), 'gi');
+            $string = $string.replace(regex, function(match){
+              return $numbers[match];
+            });
 
         }
 
         return $string;
     }
 });
-
-
