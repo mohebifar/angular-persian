2575bc43e3835c7390aaad0f5795c7158d797844
mohebifar <netfars@gmail.com>
2014-10-16 13:17:49 +0330

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'mohebifarPersianTools', function($scope, mohebifarPersianTools) {
+	//TODO - put any directive code here
+}]);
